b4b484838943546096e00f629e0d1a6e8d36fd47
ph-alaa <Alaa_lafi333@hotmail.com>
2023-07-08 23:21:03 +0300

    Add project files.

---
+++ Phil's-Lending-Library/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Phil_s_Lending_Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
